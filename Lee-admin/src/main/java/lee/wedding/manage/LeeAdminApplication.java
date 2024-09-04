f0349c9ab427fae06dab5cb43dc75a4b47fc399a
limj <limengjie@dataexa.com>
2024-09-04 22:19:18 +0800

    启动类添加开启异步按钮

--- Lee-admin/src/main/java/lee/wedding/manage/LeeAdminApplication.java
+++ Lee-admin/src/main/java/lee/wedding/manage/LeeAdminApplication.java
@@ -2,6 +2,7 @@ package lee.wedding.manage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @ ClassName LeeAdminApplication
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @ Date 2024/9/4 18:11
  **/
 @SpringBootApplication
+@EnableAsync // 开启异步事务
 public class LeeAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(LeeAdminApplication.class, args);
--- Lee-admin/src/main/java/lee/wedding/manage/controller/LeeController.java
+++ Lee-admin/src/main/java/lee/wedding/manage/controller/LeeController.java
@@ -1,5 +1,6 @@
 package lee.wedding.manage.controller;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
